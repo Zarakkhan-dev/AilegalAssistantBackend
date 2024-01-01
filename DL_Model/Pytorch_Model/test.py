77fcb901bd5cc86697d4bf1af30db7293902e363
Zarakkhan-dev <119649492+Zarakkhan-dev@users.noreply.github.com>
2024-01-01 18:46:18 +0500

    changing

--- DL_Model/Pytorch_Model/test.py
+++
@@ -1,7 +0,0 @@
-sentence = "hello this is 299 uis here"
-
-for i in range(255, 300):
-    if str(i) in sentence:
-        print(f"Number {i} found in the sentence")
-    else:
-        print(f"Number {i} not found in the sentence")
