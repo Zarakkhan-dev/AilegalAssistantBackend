d572da0b5bbeffb96bd9aa8ba4a8770d87c63800
Zarak Khan <zarakkhan23630@gmail.com>
2024-02-09 22:02:38 +0500

    azure setting

--- core/deployment.py
+++ core/deployment.py
@@ -1,13 +1,18 @@
 import os 
 from .settings import *
-from.settings import BASE_DIR
+from .settings import BASE_DIR
 
-ALLOWED_HOSTS=[os.environ["WEBSIE_HOSTNAME"]]
-CSRF_TRUSTES_ORIGINS=['https://'+os.environ['WEBSITE_HOSTNAME']]
-DEBUG=False
+
+SECRET_KEY = os.environ['SECRET']
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
+DEBUG = False
+
+# WhiteNoise configuration
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddeware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -15,17 +20,18 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-STATICFILES_STORAGE =['whitenoise.storage.CompressedManifestStaticFilesStorage']
-STATIC_ROOT=os.pardir.join(BASE_DIR,'staticfiles')
-SECRET_KEY = os.environ['Secret']
-connection_string =os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-parameters ={pair.split('='):pair.split('=')[1] for pair in connection_string.split('')}
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': parameters['dbname'],
-        'HOST':parameters['host'],
-        'USER':parameters['user'],
-        'PASSWORD':parameters['password']
+        'NAME': conn_str_params['dbname'],
+        'HOST': conn_str_params['host'],
+        'USER': conn_str_params['user'],
+        'PASSWORD': conn_str_params['password'],
     }
 }
